9c3aaca132ac00b6cae80eb9578453c5279a6ce8
Silas Berger <berger.silu@hotmail.com>
2018-02-12 19:48:25 +0100

    implemented positivity lesson

--- app/contexts/lessons/ThinkPositively.js
+++ app/contexts/lessons/ThinkPositively.js
@@ -32,8 +32,17 @@ function ThinkPositively() {
 
     this.loadMessages = function () {
         this.messages = [
-            "This is a lesson about positivity",
-            "Alright, that's all I had to say!"
+            "So, what I would like to talk to you about is positivity.",
+            "Or, in other words, how you can be kinder to yourself =)",
+            "Tell me, have you ever said something like 'I'm late again, man, I'm always late'?",
+            "Or 'That was so bad, I always make mistakes'?",
+            "A lot of us do this. Well, I guess pretty much everyone does this, haha :D",
+            "The thing is though, that's not fair!",
+            "You're not ALWAYS late, you are SOMETIMES late. And you don't ALWAYS make mistakes, you SOMETIMES make mistakes, see what I mean?",
+            "This seems like such a little difference, but you would be surprised how much it can change your attitude towards yourself!",
+            "So, the next time you miss the bus, don't say 'That's typical, I always miss the bus'. Rather say 'Okay, I sometimes miss the bus, I'll try to leave a bit earlier tomorrow'.",
+            "Sounds completely different, right?",
+            "So, that's basically the whole story of it already =)",
         ];
     };
 
